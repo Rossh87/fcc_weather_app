67e1cb9b476ed536330be8d953a90f9dc4aa129f
Ross Hunter <rossh.87@gmail.com>
2018-03-17 01:02:46 -0500

    final without imgs

--- public/interactions.js
+++ public/interactions.js
@@ -1,26 +1,64 @@
 $(document).ready(function(){
 
-	var allDataFields=document.getElementsByTagName("td");
+	function changeImg(){
+		let imgs = ['url("weather_app_imgs/spring.jpeg")\;', 'url("weather_app_imgs/summer.jpeg")\;', 'url("weather_app_imgs/autumn.jpg")\;', 'url("weather_app_imgs/winter.jpeg")\;'];
+		$("body").css("background-image", imgs[imgCounter]);
+		console.log(imgs[imgCounter])
+		if (imgCounter == 3){
+			imgCounter = 0;
+		} else {
+			imgCounter++;
+		}
+	}
+
+	var imgCounter = 0;
+
+	setInterval(function(){
+		changeImg()		
+	}, 10000);
 
 	if ("geolocation" in navigator){
 		navigator.geolocation.getCurrentPosition(function(res){
-			var latitude = res.coords.latitude;
-			var longitude = res.coords.longitude;
-				$.ajax({
-					url: "https://fcc-weather-api.glitch.me/api/current?lon="+latitude+"&lat="+longitude,
-					method: "GET",
-					success: function(data){
-						console.log(data);
-						$(allDataFields[0]).text(data.main.temp);
-						$(allDataFields[1]).text(data.main.humidity);
-						$(allDataFields[2]).text(data.wind.speed);
-					}
-				});
-			
+			let latitude = res.coords.latitude;
+			let longitude = res.coords.longitude;
+			let allDataFields=document.getElementsByTagName("td");
+			let insertTemp=document.getElementById("temp");
+			let insertWindSpeed=document.getElementById("windspeed");
+			let tempUnit = document.getElementById("tempUnit");
+			let speedUnit =document.getElementById("speedUnit");
+			let widgetBody = document.querySelector(".app-body");
+			$.ajax({
+				url: "https://fcc-weather-api.glitch.me/api/current?lon="+longitude+"&lat="+latitude,
+				method: "GET",
+				success: function (data){
+					let fahrenheit = data.main.temp*1.8 + 32;
+					let mph = (data.wind.speed/1.61).toFixed(2);
+					let metric = true;
+					$(insertTemp).text(data.main.temp);
+					$(insertWindSpeed).text(data.wind.speed);
+					$(allDataFields[3]).text(data.main.humidity + " %");
+					$(insertWindSpeed).text(data.wind.speed);
+					$(allDataFields[7]).text(data.weather[0].description);
+
+					widgetBody.addEventListener("click", function(e){
+						if (metric){
+							metric = false;
+							$(insertTemp).text(fahrenheit);
+							$(insertWindSpeed).text(mph);
+							$(tempUnit).text("F");
+							$(speedUnit).text("mph");
+						} else {
+							metric = true;
+							$(insertTemp).text(data.main.temp);
+							$(insertWindSpeed).text(data.wind.speed);
+							$(tempUnit).text("C");
+							$(speedUnit).text("kph");
+						}
+					})
+				}
+			});
 		});
 	}
-	
-
 })
 
 	
